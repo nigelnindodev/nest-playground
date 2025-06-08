f12f59cd98e5e2a4e949a6401e3fa128ec1f3604
Nigel Nindo <nigelnindodev@gmail.com>
2025-06-08 16:56:55 +0300

    Trivial example to provide mock with DI

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,19 +4,31 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
+    const mockAppService = {
+      getHello: jest.fn(() => 'Hello Mocked World!'),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: mockAppService, // You can also use a class or a factory. Factory looks to be the most robust.
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Mocked World!"', () => {
+      expect(appController.getHello()).toBe('Hello Mocked World!');
+      expect(appService.getHello).toHaveBeenCalled();
     });
   });
 });
